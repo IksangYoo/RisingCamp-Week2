ddaf040751c31bcb367e4c919ec542771b8d0fd8
Iksang Yoo <andrew4477@naver.com>
2023-01-30 18:11:47 -0500

    Initial Commit

---
+++ RisingCamp-Week2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RisingCamp-Week2
+//
+//  Created by Iksang Yoo on 2023/01/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
